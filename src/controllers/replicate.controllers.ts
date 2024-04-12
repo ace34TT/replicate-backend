fed147ded7a3a48ae94ae0a5c152b35bc956e5f6
ace34TT <tafinasoa35@gmail.com>
2024-04-12 10:09:39 +0300

    elete file

--- src/controllers/replicate.controllers.ts
+++ src/controllers/replicate.controllers.ts
@@ -365,6 +365,7 @@ export const realEsrganHandler = async (req: Request, res: Response) => {
     const framedFilename = generateRandomString(10) + ".png";
     await generateImageForPrintable(image, framedFilename);
     const imageUrl = await uploadFileToFirebase(framedFilename, "printable");
+    deleteImage(framedFilename);
     console.log(imageUrl);
     const output = await replicate.run(
       "nightmareai/real-esrgan:42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b",
