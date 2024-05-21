6c01e18a23a1d82429c4d08baf9ef938626b573b
ace34TT <tafinasoa35@gmail.com>
2024-05-21 19:57:22 +0300

    last update

--- src/routes/firebase.routes.ts
+++ src/routes/firebase.routes.ts
@@ -2,6 +2,6 @@ import express from "express";
 import { deleteUser_TV } from "../controllers/fireabase.controllers";
 const router = express.Router();
 
-router.delete("turf_visualizer/user", deleteUser_TV);
+router.delete("/turf_visualizer/user", deleteUser_TV);
 
 export { router as FirebaseRoutes };
